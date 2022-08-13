40f5702a5d41f9f717579cabcc4020f5e5dbdad8
Volodymyr <branets.volodymyr@gmail.com>
2022-08-13 20:43:02 +0300

    Add some new image, add posts.js, add posting.js, add modal window in html blog.html, change button More position to right

---
+++ js/posting.js
@@ -0,0 +1,38 @@
+const postsSection = document.querySelector(".posts");
+
+function renderPosts() {
+  posts.forEach((post) => {
+    postsSection.innerHTML += `
+        <div class="row m-3">
+            <div class="col"
+                style="background-image: url(img/${post.img}); background-size: cover; background-position: center; height: 350px;">
+            </div>
+            <div class="col">
+                <h2>${post.title}</h2>
+                <hr class="text-danger">
+                <p>${post.shortText}</p>
+                <p><small class="text-muted">Date: ${post.date}</small></p>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-danger btn-more" onclick="renderModal(${post.id})" data-bs-toggle="modal" data-bs-target="#blog-post-modal">
+                    More...
+                </button>
+            </div>
+        </div>
+      `;
+  });
+}
+renderPosts();
+
+function renderModal(id) {
+  const modal = document.querySelector("#blog-post-modal");
+  const postModal = posts.find((post) => post.id === id);
+  modal.querySelector(".modal-title").innerHTML = postModal.title;
+  modal.querySelector(".post-image").src = `img/${postModal.img}`;
+  modal.querySelector(".post-image").alt = `${postModal.img}`;
+  modal.querySelector(".post-text").innerHTML = postModal.longText;
+  modal.querySelector(".post-date").innerHTML = `Date: ${postModal.date}`;
+}
+
+// document.querySelectorAll(".btn-more").forEach((btn) => {
+//   btn.addEventListener("click");
+// });
---
+++ js/posts.js
@@ -0,0 +1,52 @@
+const posts = [
+  {
+    id: 0,
+    title: "What you can say about this?",
+    shortText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    longText:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    img: "phone-record.jpeg",
+    date: "13 Aug 2022",
+  },
+  {
+    id: 1,
+    title: "Traveling is cool",
+    shortText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    longText:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    img: "cameraman-photo2.jpeg",
+    date: "24 Sep 2021",
+  },
+  {
+    id: 2,
+    title: "Auto show in USA",
+    shortText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    longText:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    img: "autoshow.jpeg",
+    date: "5 Aug 2022",
+  },
+  {
+    id: 3,
+    title: "Car tuning for everyone",
+    shortText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    longText:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    img: "tuning-car.jpeg",
+    date: "11 Jul 2022",
+  },
+  {
+    id: 4,
+    title: "Today, tomorow and so on...",
+    shortText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    longText:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    img: "car-on-road.jpeg",
+    date: "10 Jun 2022",
+  },
+];
